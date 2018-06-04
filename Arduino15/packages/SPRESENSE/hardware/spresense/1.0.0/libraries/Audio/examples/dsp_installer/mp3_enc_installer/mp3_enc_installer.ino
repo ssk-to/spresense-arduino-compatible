6494865c227860b0f8bd1fc9c6b7e351379f9dc5
Shunichi.Kusano <Shunichi.Kusano@sony.com>
2018-06-04 14:27:39 +0900

    Add copyright header for all sketches
    refs #6768

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Audio/examples/dsp_installer/mp3_enc_installer/mp3_enc_installer.ino
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Audio/examples/dsp_installer/mp3_enc_installer/mp3_enc_installer.ino
@@ -1,22 +1,38 @@
-/* mp3_enc_installer
- *  
- * The DSP installer is used to load code for the Audio DSP into SPI-Flash.
- * This needs to be done ahead of using the Audio DSP functions in any sketch.
- * A range of Audio DSP functions are available. This skecth loads MP3 encode and decode.
- * 
- * Run the sketch and you should see
- * Installing DSPs...
- * Install: /mnt/spif/BIN/MP3ENC  Done.
- * Finished.
- * 
- * The loading process only needs to be done once as the DSP code is retained when up upload a new sketch.
- * 
- * To check what DSP files are loaded is a board you have already programmed you can use Nuttx Shell to 
- * list the files installed in the SPI-Flash. Check the directory /mnt/spif/BIN/
- * 
- * You can use dsp_inataller with a range of encoders and decoders as they become available.
- * 
- * This example code is in the public domain.
+/*
+ *  mp3_enc_installer.ino - MP3 encoder dsp installer
+ *  Copyright 2018 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  The DSP installer is used to load code for the Audio DSP into SPI-Flash.
+ *  This needs to be done ahead of using the Audio DSP functions in any sketch.
+ *  A range of Audio DSP functions are available. This skecth loads MP3 encode and decode.
+ *
+ *  Run the sketch and you should see
+ *  Installing DSPs...
+ *  Install: /mnt/spif/BIN/MP3ENC  Done.
+ *  Finished.
+ *
+ *  The loading process only needs to be done once as the DSP code is retained when up upload a new sketch.
+ *
+ *  To check what DSP files are loaded is a board you have already programmed you can use Nuttx Shell to 
+ *  list the files installed in the SPI-Flash. Check the directory /mnt/spif/BIN/
+ *
+ *  You can use dsp_inataller with a range of encoders and decoders as they become available.
+ *
+ *  This example code is in the public domain.
  */
 
 #include <stdio.h>
--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/GNSS/examples/gnss/gnss.ino
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/GNSS/examples/gnss/gnss.ino
@@ -1,11 +1,27 @@
-/* gnss
- * 
- * Spresense has an built in GNSS receiver which supports GPS and other GNSS satellites.  
- * This skecth provides an example the GNSS operation.
- * Simply upload the sketch, reset the board and check the USB serial output.
- * After 3 seconds status information should start to appear.
- * 
- * This example code is in the public domain.
+/*
+ *  gnss.ino - GNSS example application
+ *  Copyright 2018 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  Spresense has an built in GNSS receiver which supports GPS and other GNSS satellites.  
+ *  This skecth provides an example the GNSS operation.
+ *  Simply upload the sketch, reset the board and check the USB serial output.
+ *  After 3 seconds status information should start to appear.
+ *
+ *  This example code is in the public domain.
  */
 
 #include <GNSS.h>
