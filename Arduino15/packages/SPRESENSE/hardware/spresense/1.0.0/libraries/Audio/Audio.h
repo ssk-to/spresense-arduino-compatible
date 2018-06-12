7ded445f2ececdd6126e8ede1daca0b8ee1ef03b
Kamil Tomaszewski <kamil.tomaszewski@tieto.com>
2018-06-12 08:58:34 +0200

    Correct copyright for libraries.

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Audio/Audio.h
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Audio/Audio.h
@@ -1,22 +1,21 @@
 /*
-  Audio.h - Audio include file for the Sparduino SDK
-  Copyright (C) 2018 Sony Semiconductor Solutions Corp.
-  Copyright (c) 2017 Sony Corporation  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ *  Audio.h - Audio include file for the SPRESENSE SDK
+ *  Copyright 2018 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 /**
  * Audio Library for Arduino IDE on SPRESENSE.
