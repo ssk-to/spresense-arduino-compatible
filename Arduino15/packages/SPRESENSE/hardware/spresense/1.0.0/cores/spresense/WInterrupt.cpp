85d6c0a7b7e270f46ec6f93db3a892a9cd63e791
Kazuya Hioki <Kazuya.Hioki@sony.com>
2018-06-14 10:02:47 +0900

    Refactoring WInterrupt
    - Use board_gpio function instead of cxd56_gpioint
    - Change to simple code and reduce code size

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/cores/spresense/WInterrupt.cpp
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/cores/spresense/WInterrupt.cpp
@@ -20,70 +20,25 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <sdk/config.h>
+#include <arch/board/board.h>
+#include <arch/chip/pin.h>
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
 #include <common/up_arch.h>
-#include <arch/cxd56xx/irq.h>
+#include <chip/cxd5602_memorymap.h>
 #include <chip/cxd5602_topreg.h>
-#include <cxd56_gpio.h>
-extern "C" {
-#include <cxd56_gpioint.h>
-}
+
 #include <Arduino.h>
 #include "utility.h"
 #include "wiring.h"
 #include "wiring_private.h"
 
 #ifndef CONFIG_CXD56_GPIO_IRQ
-# error Please enable GPIO interrupt support in Nuttx
+# error Please enable GPIO interrupt support in NuttX
 #endif // CONFIG_CXD56_GPIO_IRQ
 
 static irqstate_t s_irq_flags;
-
-typedef struct __interrupt_map {
-    uint8_t pin;
-    int irq;
-    void (*isr)(void);
-} interrupt_map_t;
-
-/* irq number is allocated dynamically */
-static interrupt_map_t s_irq_maps[] = {
-    { PIN_UART2_RXD,     -1, 0 },
-    { PIN_UART2_TXD,     -1, 0 },
-    { PIN_HIF_IRQ_OUT,   -1, 0 },
-    { PIN_PWM3,          -1, 0 },
-    { PIN_SPI2_MOSI,     -1, 0 },
-    { PIN_PWM1,          -1, 0 },
-    { PIN_PWM0,          -1, 0 },
-    { PIN_SPI3_CS1_X,    -1, 0 },
-    { PIN_SPI2_MISO,     -1, 0 },
-    { PIN_PWM2,          -1, 0 },
-    { PIN_SPI4_CS_X,     -1, 0 },
-    { PIN_SPI4_MOSI,     -1, 0 },
-    { PIN_SPI4_MISO,     -1, 0 },
-    { PIN_SPI4_SCK,      -1, 0 },
-    { PIN_SPI3_SCK,      -1, 0 },
-    { PIN_SPI3_MISO,     -1, 0 },
-    { PIN_SPI3_MOSI,     -1, 0 },
-    { PIN_SPI3_CS0_X,    -1, 0 },
-    { PIN_SPI0_MOSI,     -1, 0 },
-    { PIN_SPI0_MISO,     -1, 0 },
-    { PIN_I2C0_BDT,      -1, 0 },
-    { PIN_I2C0_BCK,      -1, 0 },
-    { PIN_EMMC_DATA0,    -1, 0 },
-    { PIN_EMMC_DATA1,    -1, 0 },
-    { PIN_I2S0_DATA_OUT, -1, 0 },
-    { PIN_I2S0_DATA_IN,  -1, 0 },
-    { PIN_EMMC_DATA2,    -1, 0 },
-    { PIN_EMMC_DATA3,    -1, 0 },
-    { PIN_SEN_IRQ_IN,    -1, 0 },
-    { PIN_EMMC_CLK,      -1, 0 },
-    { PIN_EMMC_CMD,      -1, 0 },
-    { PIN_I2S0_LRCK,     -1, 0 },
-    { PIN_I2S0_BCK,      -1, 0 },
-    { PIN_UART2_CTS,     -1, 0 },
-    { PIN_UART2_RTS,     -1, 0 },
-};
 
 #define INTC_EN(n) (CXD56_INTC_BASE + 0x10 + (((n) >> 5) << 2))
 
@@ -101,92 +56,51 @@ static bool irq_enabled(int irq)
     return enabled;
 }
 
-static int get_slot_by_pin(uint8_t pin)
+static void attach_interrupt(uint8_t pin, void (*isr)(void), int mode)
 {
-    arrayForEach(s_irq_maps, i) {
-        if (s_irq_maps[i].pin == pin)
-            return i;
-    }
+    int  _mode;
+    bool filter = true; // always enable noise filter
 
-    return -1;
-}
-
-static int get_slot_by_irq(int irq)
-{
-    if (irq < 0) return -1;
-
-    arrayForEach(s_irq_maps, i) {
-        if (s_irq_maps[i].irq == irq)
-            return i;
-    }
-
-    return -1;
-}
-
-static inline bool is_valid_mode(int mode) inline_function;
-static inline bool is_valid_mode(int mode)
-{
-    return (mode == LOW || mode == HIGH || mode == CHANGE || mode == RISING || mode == FALLING);
-}
-
-static int interrupt_handler(int irq, FAR void* context, FAR void *arg)
-{
-    //printf("handle GPIO interrupt [%d]\n", irq);
-    unuse(context);
-    unuse(arg);
-
-    int slot = get_slot_by_irq(irq);
-    if (slot >= 0 && s_irq_maps[slot].isr)
-        s_irq_maps[slot].isr();
-
-    return 0;
-}
-
-static void attach_interrupt(int slot, void (*isr)(void), int mode)
-{
-    if (slot < 0 || !isr || !is_valid_mode(mode)) {
-        printf("ERROR: Invalid arg detected. slot [%d], isr [0x%08x], mode [%d]\n", slot, isr, mode);
+    switch (mode) {
+    case LOW:
+        _mode = INT_LOW_LEVEL;
+        break;
+    case HIGH:
+        _mode = INT_HIGH_LEVEL;
+        break;
+    case CHANGE:
+        _mode = INT_BOTH_EDGE;
+        break;
+    case RISING:
+        _mode = INT_RISING_EDGE;
+        break;
+    case FALLING:
+        _mode = INT_FALLING_EDGE;
+        break;
+    default:
+        printf("ERROR: unknown interrupt mode [%d]\n", mode);
         return;
     }
 
-    uint32_t _mode = mode == LOW    ? GPIOINT_LEVEL_LOW  :
-                     mode == HIGH   ? GPIOINT_LEVEL_HIGH :
-                     mode == CHANGE ? GPIOINT_EDGE_BOTH  :
-                     mode == RISING ? GPIOINT_PSEUDO_EDGE_RISE  : GPIOINT_PSEUDO_EDGE_FALL;
-
-    noInterrupts();
-    int irq = cxd56_gpioint_config(s_irq_maps[slot].pin,
-                                   GPIOINT_NOISE_FILTER_ENABLE |
-                                   _mode,
-                                   interrupt_handler);
-    //printf("cxd56_gpioint_config returns [%d]\n", irq);
-
+    int irq = board_gpio_intconfig(pin, _mode, filter, (xcpt_t)isr);
     if (irq < 0) {
-        interrupts();
         printf("ERROR: Out of interrupt resources\n");
         return;
     }
 
     /* wait RTC few cycles before the interrupt is enabled for noise filter. */
     delay(1);
-    s_irq_maps[slot].irq = irq;
-    s_irq_maps[slot].isr = isr;
-    cxd56_gpioint_enable(s_irq_maps[slot].pin);
-    interrupts();
+    board_gpio_int(pin, true);
 }
 
-static void detach_interrupt(int slot)
+static void detach_interrupt(uint8_t pin)
 {
-    if (slot < 0) {
-        printf("ERROR: Invalid slot [%d]\n", slot);
+    int irq = board_gpio_int(pin, false);
+    if (irq < 0) {
+        printf("ERROR: Invalid pin [%d]\n", pin);
         return;
     }
-
-    noInterrupts();
-    cxd56_gpioint_config(s_irq_maps[slot].pin, 0, NULL);
-    s_irq_maps[slot].irq = -1;
-    s_irq_maps[slot].isr = 0;
-    interrupts();
+    board_gpio_intconfig(pin, 0, false, NULL);
 }
 
 extern "C" {
@@ -226,14 +140,16 @@ void irq_restore(uint16_t flags)
 
 void attachInterrupt(uint8_t interrupt, void (*isr)(void), int mode)
 {
-    uint8_t pin = pin_convert(interrupt);
-    int slot = get_slot_by_pin(pin);
-    attach_interrupt(slot, isr, mode);
+    uint8_t _pin = pin_convert(interrupt);
+    if (_pin == PIN_NOT_ASSIGNED)
+        return;
+    attach_interrupt(_pin, isr, mode);
 }
 
 void detachInterrupt(uint8_t interrupt)
 {
-    uint8_t pin = pin_convert(interrupt);
-    int slot = get_slot_by_pin(pin);
-    detach_interrupt(slot);
+    uint8_t _pin = pin_convert(interrupt);
+    if (_pin == PIN_NOT_ASSIGNED)
+        return;
+    detach_interrupt(_pin);
 }
