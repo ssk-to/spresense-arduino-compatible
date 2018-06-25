f9cf5b46150738c1ff981b97e8f7a36b1bece769
Pawel Dogiel <pawel.dogiel@tieto.com>
2018-06-25 12:35:37 +0200

    Add doxygen comments to Camera examples

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Camera/examples/camera/camera.ino
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/libraries/Camera/examples/camera/camera.ino
@@ -9,12 +9,12 @@
  *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *  Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  *  This is a test app for the camera library.
  *  This library can only be used on the Spresense with the FCBGA chip package.
@@ -29,12 +29,18 @@ SDClass theSD;
 
 CameraClass theCamera;
 
+/**
+ * @brief Initialize camera
+ */
 void setup() {
   puts("Prepare camera");
   theCamera.begin();
   theCamera.initialize();
 }
 
+/**
+ * @brief Save picture to file in one minute interval
+ */
 void loop() {
   static int  cnt = 0;
   static char filename[14] = {0};
