78e62e22069970e4fd74026413027725f3b03df8
Kazuya Hioki <Kazuya.Hioki@sony.com>
2018-11-14 17:36:37 +0900

    Replace obsolete name
    refs #7803

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/cores/spresense/wiring_analog.c
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/cores/spresense/wiring_analog.c
@@ -1,5 +1,5 @@
 /*
-  wiring_analog.c - analog I/O for the Sparduino SDK
+  wiring_analog.c - analog I/O for the Spresense SDK
   Copyright (C) 2018 Sony Semiconductor Solutions Corp.
   Copyright (c) 2017 Sony Corporation  All right reserved.
 
